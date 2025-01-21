863aa94f38755b2cf8379ace69da3377ea769e68
Eduard Kosenko <eduard.kosenko@georgebrown.ca>
2025-01-21 11:12:27 -0500

    edited lab03.py as per instructions

--- lab03.py
+++ lab03.py
@@ -1,13 +1,48 @@
 import random
+from math import trunc
 
 # Setup
 diceOptions = list(range(1, 7))
 weapons = ['Fist', 'Knife', 'Club', 'Gun', 'Bomb', 'Nuclear Bomb']
-print("Available Weapons:", ', '.join(weapons))
+# print("Available Weapons:", ', '.join(weapons))
+print('Available weapons:')
+for weapon in weapons:
+    print(weapon)
 
 # Inputs
-combatStrength = max(1, min(6, int(input("Hero strength (1-6): "))))
-mCombatStrength = max(1, min(6, int(input("Monster strength (1-6): "))))
+# combatStrength = max(1, min(6, int(input("Hero strength (1-6): "))))
+# mCombatStrength = max(1, min(6, int(input("Monster strength (1-6): "))))
+
+combatStrength = -1
+while combatStrength == -1:
+    try:
+        usrInput = int(input("Choose hero strength (1-6): "))
+        if usrInput in diceOptions:
+            combatStrength = usrInput
+        else:
+            raise IndexError
+    except IndexError:
+        print("Please enter a number between 1-6")
+        continue
+    except ValueError:
+        print("Please enter a number.")
+        continue
+
+mCombatStrength = -1
+while mCombatStrength == -1:
+    try:
+        usrInput = int(input("Choose monster strength (1-6): "))
+        if usrInput in diceOptions:
+            mCombatStrength = usrInput
+        else:
+            raise IndexError
+    except IndexError:
+        print("Please enter a number between 1-6")
+        continue
+    except ValueError:
+        print("Please enter a number.")
+        continue
+
 
 # Battle
 for j in range(1, 21, 2):
